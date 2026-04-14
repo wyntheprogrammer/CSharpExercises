d7b254a2b72a26db78cf89faca09b84186292f15
Lemuell <lemuell@gmail.com>
2026-04-14 13:33:29 +0800

    Added BasicExercises 2

---
+++ BasicExercises2/BasicExercises2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             
+               2.Sum of Two Numbers
+
+               Write a C# Sharp program to print the sum of two numbers. 
+
+            */
+
+            // Approach 1:
+            Console.WriteLine($"The sum of two numbers is: {1 + 2}");
+
+            // Approach 2:
+            int a = 1;
+            int b = 2;
+            int sum = a + b;
+
+            Console.WriteLine($"The sum of two numbers is: {sum} ");
+        }
+    }
+}
